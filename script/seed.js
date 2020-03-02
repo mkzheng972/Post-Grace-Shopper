437668fdde323383212fbc200ba21eca6eb2d3df
Yan <ing.yangao@gmail.com>
2020-03-02 18:56:21 -0500

    added 20 users

--- script/seed.js
+++ script/seed.js
@@ -1,5 +1,148 @@
 'use strict'
 
+const dummyUsers = [
+  {
+    firstName: 'Deanna',
+    lastName: 'Aynold',
+    email: 'daynold0@cisco.com',
+    imageUrl: 'http://dummyimage.com/119x183.bmp/cc0000/ffffff',
+    password: 'CDQ2CbQ6KRT'
+  },
+  {
+    firstName: 'Chip',
+    lastName: 'Wabersich',
+    email: 'cwabersich1@huffingtonpost.com',
+    imageUrl: 'http://dummyimage.com/184x158.png/ff4444/ffffff',
+    password: 'K6VeYQJQOgM'
+  },
+  {
+    firstName: 'Denni',
+    lastName: 'Fosher',
+    email: 'dfosher2@google.it',
+    imageUrl: 'http://dummyimage.com/225x226.png/dddddd/000000',
+    password: '751zUPyt'
+  },
+  {
+    firstName: 'Abbott',
+    lastName: 'Janos',
+    email: 'ajanos3@imdb.com',
+    imageUrl: 'http://dummyimage.com/241x229.jpg/dddddd/000000',
+    password: 'jyfTqZDa'
+  },
+  {
+    firstName: 'Vilma',
+    lastName: 'Dufaire',
+    email: 'vdufaire4@china.com.cn',
+    imageUrl: 'http://dummyimage.com/177x221.png/ff4444/ffffff',
+    password: '6eCxcZatAZu'
+  },
+  {
+    firstName: 'Karil',
+    lastName: 'Jakubowski',
+    email: 'kjakubowski5@soundcloud.com',
+    imageUrl: 'http://dummyimage.com/239x178.jpg/dddddd/000000',
+    password: 'AQI9GtF'
+  },
+  {
+    firstName: 'Harmony',
+    lastName: 'Hollyer',
+    email: 'hhollyer6@reference.com',
+    imageUrl: 'http://dummyimage.com/205x195.bmp/dddddd/000000',
+    password: '5AwnTq'
+  },
+  {
+    firstName: 'Jeannie',
+    lastName: 'McGaraghan',
+    email: 'jmcgaraghan7@livejournal.com',
+    imageUrl: 'http://dummyimage.com/130x230.bmp/5fa2dd/ffffff',
+    password: 'rEPSGDE0i88'
+  },
+  {
+    firstName: 'Pooh',
+    lastName: 'Zemler',
+    email: 'pzemler8@qq.com',
+    imageUrl: 'http://dummyimage.com/208x170.bmp/dddddd/000000',
+    password: 'MJYfaf'
+  },
+  {
+    firstName: 'Alec',
+    lastName: 'Bartalot',
+    email: 'abartalot9@examiner.com',
+    imageUrl: 'http://dummyimage.com/240x111.bmp/dddddd/000000',
+    password: 's5fB8el7oa'
+  },
+  {
+    firstName: 'Cece',
+    lastName: 'Haylands',
+    email: 'chaylandsa@state.gov',
+    imageUrl: 'http://dummyimage.com/237x191.bmp/cc0000/ffffff',
+    password: 'AgSybaoj60X'
+  },
+  {
+    firstName: 'Forrester',
+    lastName: 'Westley',
+    email: 'fwestleyb@fastcompany.com',
+    imageUrl: 'http://dummyimage.com/175x142.jpg/cc0000/ffffff',
+    password: '34wwb1'
+  },
+  {
+    firstName: 'Mack',
+    lastName: 'Dallemore',
+    email: 'mdallemorec@skyrock.com',
+    imageUrl: 'http://dummyimage.com/226x211.bmp/ff4444/ffffff',
+    password: 'Q7MLWLeWvM1m'
+  },
+  {
+    firstName: 'Milicent',
+    lastName: 'Priestland',
+    email: 'mpriestlandd@360.cn',
+    imageUrl: 'http://dummyimage.com/119x220.jpg/ff4444/ffffff',
+    password: 'pw4qd8O7'
+  },
+  {
+    firstName: 'Linn',
+    lastName: 'Frowde',
+    email: 'lfrowdee@ezinearticles.com',
+    imageUrl: 'http://dummyimage.com/235x153.png/cc0000/ffffff',
+    password: 'Q5oVYoh1geT'
+  },
+  {
+    firstName: 'Natka',
+    lastName: 'Dudliston',
+    email: 'ndudlistonf@oracle.com',
+    imageUrl: 'http://dummyimage.com/204x156.png/5fa2dd/ffffff',
+    password: '0yAkBQqqABkT'
+  },
+  {
+    firstName: 'Elle',
+    lastName: 'Chippindale',
+    email: 'echippindaleg@webmd.com',
+    imageUrl: 'http://dummyimage.com/178x241.jpg/5fa2dd/ffffff',
+    password: 'rxrEcwjYtnx9'
+  },
+  {
+    firstName: 'Briana',
+    lastName: 'Brolechan',
+    email: 'bbrolechanh@infoseek.co.jp',
+    imageUrl: 'http://dummyimage.com/112x244.bmp/dddddd/000000',
+    password: 'XOUTDb7pe'
+  },
+  {
+    firstName: 'Chester',
+    lastName: 'Rougier',
+    email: 'crougieri@webmd.com',
+    imageUrl: 'http://dummyimage.com/207x248.jpg/ff4444/ffffff',
+    password: 'lPKUhZ2pk'
+  },
+  {
+    firstName: 'Cynthy',
+    lastName: 'Jonah',
+    email: 'cjonahj@tuttocitta.it',
+    imageUrl: 'http://dummyimage.com/127x104.bmp/dddddd/000000',
+    password: '4RECgOgtf6'
+  }
+]
+
 const db = require('../server/db')
 const {User, Order, Noodle, Ingredient} = require('../server/db/models')
 
@@ -9,7 +152,10 @@ async function seed() {
 
   const users = await Promise.all([
     User.create({email: 'cody@email.com', password: '123'}),
-    User.create({email: 'murphy@email.com', password: '123'})
+    User.create({email: 'murphy@email.com', password: '123'}),
+    dummyUsers.map(user => {
+      return User.create(user)
+    })
   ])
 
   const user = await User.create({
