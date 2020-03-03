ff3c53d0b7c5dd02e6a49b9ccfbb612a8a1419a8
mkzheng972 <markzheng27@gmail.com>
2020-03-03 11:45:58 -0500

    worked on singleNoodle Component and added backend noodle routes

--- client/components/SingleNoodle.js
+++ client/components/SingleNoodle.js
@@ -1,14 +1,27 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
+import {getSingleNoodle} from '../store/noodles'
 
 export class SingleNoodle extends Component {
   render() {
-    return <div />
+    const {name, imageUrl, description, price} = this.props.noodle
+    return (
+      <div>
+        <h1>Name: {name}</h1>
+        <img src={imageUrl} />
+        <h3>{description}</h3>
+        <h3>Price: {price}</h3>
+      </div>
+    )
   }
 }
 
-const mapStateToProps = state => ({})
+const mapStateToProps = state => ({
+  noodle: state.noodle
+})
 
-const mapDispatchToProps = {}
+const mapDispatchToProps = dispatch => ({
+  getSingleNoodle: id => dispatch(getSingleNoodle(id))
+})
 
 export default connect(mapStateToProps, mapDispatchToProps)(SingleNoodle)
